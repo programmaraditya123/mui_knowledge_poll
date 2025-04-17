ab7c3d418aad911a2fd3c921bc7beb99d51468da
programmaraditya123 <officialadi4255@gmail.com>
2025-04-17 16:17:32 +0530

    socket api

--- frontend/src/Socket/Socket.jsx
+++ frontend/src/Socket/Socket.jsx
@@ -1,8 +1,8 @@
 // socket.js
 import { io } from "socket.io-client";
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_SOCKET_URL = import.meta.env.VITE_SOCKET_BASE_URL;
 
-const socket = io(`${BASE_URL}`, {
+const socket = io(`${BASE_SOCKET_URL}`, {
   withCredentials: true,
   autoConnect: true,
 });
